00ccb2b6726c0f446d88737c927efed4a0201d9c
Angelinajf <angelinafernandeslinda@gmail.com>
2025-06-24 16:01:35 -0300

    conexao

--- conexao.php
+++ conexao.php
@@ -1,12 +1,9 @@
 <?php
 
-$host = "localhost";
-$db = "loja";
-$user = "root";
-$pass = "";
-$conexao = mysqli_connect($host, $user, $pass, $db);
-
-if($conexao->connect_error) {
-    die("Erro ao conectar" . $conexao->connect_error);
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=lojaonline;charset=utf8', 'root', '');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro ao conectar: " . $e->getMessage());
 }
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php 
+    require("conexao.php");
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
