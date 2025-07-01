18cb3e73a5c190fd5e7405315f6a1ecb4c0d11a4
Angelinajf <angelinafernandeslinda@gmail.com>
2025-07-01 16:20:05 -0300

    atualizar.php 3

--- editar.php
+++ editar.php
@@ -1,33 +1,36 @@
 <?php
 include 'conexao.php';
-$id = $_POST['id'];
+$id = $_POST['btnEditar'];
 $resultado = $pdo->prepare("SELECT * FROM produto WHERE id=?");
 $resultado->execute([$id]);
-$linha= $resultado->fetch(PDO::FETCH_ASSOC);
+$linha = $resultado->fetch(PDO::FETCH_ASSOC);
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=
      initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-<h1>Editar produto</h1>
-form action="atualizar.php" method="post">
-<input type="hidden"name="id"value="<?php echo$linha['id'] ?>">
+    <h1>Editar produto</h1>
+    <form action="atualizar.php" method="post">
+        <input type="hidden" name="id" value="<?php echo $linha['id'] ?>">
 
 
-<p>Nome</p>
-<input type="text"name="nome" value="<?php echo$linha['nome'] ?>">
+        <p>Nome</p>
+        <input type="text" name="nome" value="<?php echo $linha['nome'] ?>">
 
-<p>preco</p>
-<input type="hidden"name="id"value="<?php echo$linha['preco'] ?>">
+        <p>preco</p>
+        <input type="text" name="preco" value="<?php echo $linha['preco'] ?>">
 
-<input type="submit" value="Atualizar">
+        <input type="submit" value="Atualizar">
 
-
+    </form>
 </body>
+
 </html>
--- produto.php
+++ produto.php
@@ -1,7 +1,7 @@
 <?php
 require("conexao.php");
 
-if(isset($_POST['btnApagar'])){
+if (isset($_POST['btnApagar'])) {
     $id = $_POST['btnApagar'];
     $sql = "DELETE FROM Produto WHERE id = $id";
     $result = $conexao->query($sql);
@@ -11,16 +11,18 @@ if(isset($_POST['btnApagar'])){
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <title>Produtos</title>
 </head>
+
 <body>
     <h1>Listagem de produtos</h1>
 
-    <table border=1 >
+    <table border=1>
         <tr>
             <th>ID</th>
             <th>Nome</th>
@@ -28,30 +30,30 @@ if(isset($_POST['btnApagar'])){
             <th>Editar</th>
             <th>Apagar</th>
         </tr>
-        <?php 
+        <?php
         $resultado = $pdo->query("SELECT * FROM produto");
-        while($linha = $resultado->fetch(PDO::FETCH_ASSOC)):?>
-        <tr>
-        <td><?php echo $linha['id'] ?></td>
-        <td><?php echo $linha['nome'] ?></td>
-        <td><?php echo "R$ " . $linha ['preco'] ?></td>
-        <td>Editar</td>
-         <td>
-        <form action="editar.php" method="POST">
-        <button type="submit" name="btneditar" value="<?php echo $linha['id'] ?>">editar</button>
-         </form>
-         </td>
-         <td>
-        <form action="excluir.php" method="POST">
-        <button type="submit" name="btnApagar" value="<?php echo $linha['id'] ?>">apagar</button>
-        <form>
-        </td>
-        </tr>
+        while ($linha = $resultado->fetch(PDO::FETCH_ASSOC)): ?>
+            <tr>
+                <td><?php echo $linha['id'] ?></td>
+                <td><?php echo $linha['nome'] ?></td>
+                <td><?php echo "R$ " . $linha['preco'] ?></td>
+                <td>
+                    <form action="editar.php" method="POST">
+                        <button type="submit" name="btnEditar" value="<?php echo $linha['id'] ?>">editar</button>
+                    </form>
+                </td>
+                <td>
+                    <form action="excluir.php" method="POST">
+                        <button type="submit" name="btnApagar" value="<?php echo $linha['id'] ?>">apagar</button>
+                    </form>
+                </td>
+            </tr>
         <?php endwhile; ?>
     </table>
     <form action="cadProduto.php" method="">
-    <button type="submit">Novo Produto</button>
+        <button type="submit">Novo Produto</button>
     </form>
 
 </body>
+
 </html>
